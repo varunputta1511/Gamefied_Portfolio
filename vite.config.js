c23bca08f109577df38a82ed551487a9bc1896ae
Varun Putta <78648795+varunputta1511@users.noreply.github.com>
2025-03-09 09:49:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react"; // remove if you're not using react
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/new-2d-portfolio",
-  plugins: [react()],
+  base: "Gamefied_Portfolio",
+  plugins: [react()], // remove if you're not using react
 });
+
